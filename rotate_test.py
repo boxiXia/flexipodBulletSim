f835cee9eff4963b61f27b6b09980a2422dfead9
Boxi Xia <bx2150@columbia.edu>
2020-10-26 14:40:11 -0400

    clean up

--- rotate_test.py
+++ rotate_test.py
@@ -7,85 +7,94 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 PI = np.pi
-TWO_PI = 2 * np.pi
+TWO_PI = 1. * np.pi
 
 physicsClient = p.connect(p.GUI)  # or p.DIRECT for non-graphical version
 p.setAdditionalSearchPath(pybullet_data.getDataPath())  # optionally
-p.setGravity(0, 0, -10)
+p.setGravity(0, 0, -9.8)
 planeId = p.loadURDF("plane.urdf")
-p.changeDynamics(planeId, -1, contactStiffness=1500, contactDamping=30)
+# p.changeDynamics(planeId, -1, contactStiffness=1500, contactDamping=30)
 cubeStartPos = [0, 0, 0.24]
 cubeStartOrientation = p.getQuaternionFromEuler([0, 0, 0])
 leg_angle_arr = np.arange(0,31,2)
 result = []
 initial_steps = 480
-sim_steps = 14400
+
+dt = 1/240
+sample_dt = 10. # interval between each measurement in seconds
+
+sim_steps = int(sample_dt/dt*10)
+
+sample_step = sample_dt/dt # in steps
+num_samples = int(sim_steps/sample_step-1)
+
 
 mode = p.POSITION_CONTROL
 maxforce = [3, 3, 3, 3]
 
-w = 1.5 * PI
+w = 2. * PI
 
 for leg_angle in leg_angle_arr:
     p.resetSimulation()
-    p.setGravity(0, 0, -10)
+    p.setGravity(0, 0, -9.8)
     planeId = p.loadURDF("plane.urdf")
-    p.changeDynamics(planeId, -1, contactStiffness=1500, contactDamping=30)
+    # p.changeDynamics(planeId, -1, contactStiffness=1500, contactDamping=30)
     boxId = p.loadURDF(f"same_leg_urdf/{leg_angle}_degree.urdf", cubeStartPos, cubeStartOrientation)
     print(leg_angle)
-    T_pos = [0 for _ in range(4)]
-    T_real = [0 for _ in range(4)]
+    T_pos = np.zeros(4)
+    T_real = np.zeros(4)
     num = 0
-    velocity = [0 for _ in range(5)]
+    velocity = np.zeros(num_samples)
     distance = 0
     EulerStart_tmp = 0
 
     p.setJointMotorControlArray(boxId, [0, 1, 2, 3], controlMode=mode,
                                 targetPositions=[0, -PI, -PI, 0])  # turn
 
-    for i in range(480):
+    for i in range(int(1/dt)):
         p.stepSimulation()
-        #time.sleep(1. / 240.)
+        # time.sleep(1./480.)
     PosStart, OrnStart = p.getBasePositionAndOrientation(boxId)
     EulerStart = p.getEulerFromQuaternion(OrnStart)
 
-    for i in range(43200):
+    for i in range(sim_steps):
         p.stepSimulation()
+        t = dt*i
         # turn
-        T_pos[0] = w / 240 * i
-        T_pos[1] = w / 240 * i - math.pi
-        T_pos[2] = w / 240 * i - math.pi
-        T_pos[3] = w / 240 * i
+        T_pos[0] = w*t
+        T_pos[1] = w*t - math.pi
+        T_pos[2] = w*t - math.pi
+        T_pos[3] = w*t
         p.setJointMotorControlArray(boxId, [0, 1, 2, 3], controlMode=mode,
                                     targetPositions=[T_pos[0], T_pos[1], T_pos[2], T_pos[3]], forces=maxforce)
-        #time.sleep(1. / 480.)
-        if i == 7199: # start calculate after 30 seconds
+        # time.sleep(1. / 480.)
+        if i == sample_step-1: # start calculate after 30 seconds
             PosStart, OrnStart = p.getBasePositionAndOrientation(boxId)
             EulerStart = p.getEulerFromQuaternion(OrnStart)
             EulerStart_tmp = EulerStart[2]
-        if i >= 7200:
+        if i >= sample_step:
             PosTest, OrnTest = p.getBasePositionAndOrientation(boxId)
             EulerTest = p.getEulerFromQuaternion(OrnTest)
             if EulerTest[2] * EulerStart_tmp < 0:
                 if EulerStart_tmp <= 0:
-                    distance += PI - abs(EulerStart_tmp) + 0.001
+                    distance += PI - abs(EulerStart_tmp) + 0.00001
                     EulerStart_tmp = PI
                 else:
                     distance += EulerStart_tmp
-                    EulerStart_tmp = -0.001
+                    EulerStart_tmp = -0.00001
 
-        if (((i + 1) % 7200 == 0) and i > 7200):
+        if (((i + 1) % sample_step == 0) and i > sample_step):
             PosTest, OrnTest = p.getBasePositionAndOrientation(boxId)
             EulerTest = p.getEulerFromQuaternion(OrnTest)
             if EulerStart_tmp < 0:
-                velocity[num] = (abs(EulerTest[2] - EulerStart_tmp) + distance + 0.001) / 30
+                velocity[num] = (abs(EulerTest[2] - EulerStart_tmp) + distance + 0.00001) / sample_dt
             else:
-                velocity[num] = (abs(EulerTest[2] - EulerStart_tmp) + distance) / 30
+                velocity[num] = (abs(EulerTest[2] - EulerStart_tmp) + distance) / sample_dt
             EulerStart_tmp = EulerTest[2]
             distance = 0
             num += 1
 
-    for i in range(5):
+    for i in range(num_samples):
         print(velocity[i])
     result.extend([(leg_angle, v) for v in velocity])
 
--- velocity_test.py
+++ velocity_test.py
@@ -12,6 +12,7 @@ plt.style.use('seaborn-whitegrid')
 PI = np.pi
 TWO_PI = 2 * np.pi
 
+
 class WalkingTrot():
     """
   trotting gait
@@ -23,7 +24,8 @@ class WalkingTrot():
                  w=0.1 * np.pi,  # average angular velocity [rad/s]
                  s=0.5,  # stance ratio = stance_time/cycle_time [unitless]
                  contact_angle=120 / 180 * PI,  # contact_angle [rad]
-                 p_stance_mid=1. * np.pi,  # angle at the middle of the stance phase [rad]
+                 # angle at the middle of the stance phase [rad]
+                 p_stance_mid=1. * np.pi,
                  t=0,  # time at current step
                  ):
         this.ot = ot  # normalized time offset [0-1], [unitless]
@@ -40,9 +42,10 @@ class WalkingTrot():
         this.T = 2 * np.pi / this.w  # cycle time
 
         this.s = s  # stance ratio = stance_time/cycle_time [unitless]
-        this.ws = this.c / (this.s)  # w_stance, (time normalized) average angular velocity of stance phase [rad]
+        # w_stance, (time normalized) average angular velocity of stance phase [rad]
+        this.ws = this.c / (this.s)
         this.wa = (TWO_PI - this.c) / (
-                    1 - this.s)  # w_air, (time normalized) average angular velocity of air phase [rad]
+            1 - this.s)  # w_air, (time normalized) average angular velocity of air phase [rad]
 
         this.p_stance_high = p_stance_mid + this.c / 2.0
         #print(np.rad2deg(this.p_stance_low), np.rad2deg(p_stance_mid), np.rad2deg(this.p_stance_high))
@@ -51,7 +54,8 @@ class WalkingTrot():
         if this.ot < this.s:  # if normalized time offset < stance ratio
             this.pos = this.p_stance_low + this.c / this.s * this.ot
         else:
-            this.pos = this.p_stance_low + this.c + (TWO_PI - this.c) / (1 - this.s) * (this.ot - this.s)
+            this.pos = this.p_stance_low + this.c + \
+                (TWO_PI - this.c) / (1 - this.s) * (this.ot - this.s)
 
     #     @njit
     #     def UpdateStanceRatio(this,s):
@@ -78,8 +82,10 @@ class WalkingTrot():
         """
         update the stance ratio s (=: stance_time/cycle_time [unitless])
         """
-        ws = this.c / (s)  # w_stance, (time normalized) average angular velocity of stance phase [rad]
-        wa = (TWO_PI - this.c) / (1 - s)  # w_air, (time normalized) average angular velocity of air phase [rad]
+        ws = this.c / \
+            (s)  # w_stance, (time normalized) average angular velocity of stance phase [rad]
+        # w_air, (time normalized) average angular velocity of air phase [rad]
+        wa = (TWO_PI - this.c) / (1 - s)
         # update the converted (normalized) time this.tc
 
         this.s = s  # stance ratio = stance_time/cycle_time [unitless]
@@ -89,7 +95,8 @@ class WalkingTrot():
         if this.tc < this.s:  # 0-s
             this.pos = (this.p_stance_low + this.ws * this.tc) % TWO_PI
         else:  # s-1
-            this.pos = (this.p_stance_low + this.c + this.wa * (this.tc - this.s)) % TWO_PI
+            this.pos = (this.p_stance_low + this.c +
+                        this.wa * (this.tc - this.s)) % TWO_PI
 
     def GetPos(this, t, w=None, s=None):
         """
@@ -101,14 +108,17 @@ class WalkingTrot():
             this.UpdateStanceRatio(s)
         dt = t - this.t  # differenct in raw time
         this.t = t
-        dnt = this.w * dt / TWO_PI  # normalized phase difference [0-1][unitless]
-        this.tc = (this.tc + dnt) % 1.0  # # current converted time [0-1,unitless]
+        # normalized phase difference [0-1][unitless]
+        dnt = this.w * dt / TWO_PI
+        # # current converted time [0-1,unitless]
+        this.tc = (this.tc + dnt) % 1.0
 
         # todo sign
         if this.tc < this.s:  # 0-s
             this.pos = (this.p_stance_low + this.ws * this.tc) % TWO_PI
         else:  # s-1
-            this.pos = (this.p_stance_low + this.c + this.wa * (this.tc - this.s)) % TWO_PI
+            this.pos = (this.p_stance_low + this.c +
+                        this.wa * (this.tc - this.s)) % TWO_PI
         return this.pos
 
     def _UpdateAngularVelocity(this, t, w):
@@ -158,27 +168,22 @@ class WalkingTrot():
 # ]
 
 
-# t_arr = np.linspace(0,4,1000)
-# w_arr = PI*np.ones_like(t_arr)
-# s_arr = np.linspace(0.5,0.5,1000)
-#
-# angle1 = np.array([gaits[0].GetPos(t, w, s) for t, w, s in zip(t_arr, w_arr, s_arr)])
-# angle2 = np.array([gaits[1].GetPos(t, w, s) for t, w, s in zip(t_arr, w_arr, s_arr)])
-# angle3 = np.array([gaits[2].GetPos(t, w, s) for t, w, s in zip(t_arr, w_arr, s_arr)])
-# angle4 = np.array([gaits[3].GetPos(t, w, s) for t, w, s in zip(t_arr, w_arr, s_arr)])
-
-
 physicsClient = p.connect(p.GUI)  # or p.DIRECT for non-graphical version
 p.setAdditionalSearchPath(pybullet_data.getDataPath())  # optionally
 p.setGravity(0, 0, -10)
 planeId = p.loadURDF("plane.urdf")
-p.changeDynamics(planeId, -1, contactStiffness=1500, contactDamping=30)
+# p.changeDynamics(planeId, -1, contactStiffness=1500, contactDamping=30)
 cubeStartPos = [0, 0, 0.24]
 cubeStartOrientation = p.getQuaternionFromEuler([0, 0, 0])
-leg_angle_arr = np.arange(0,31,1)
+leg_angle_arr = np.arange(0, 31, 2)
 result = []
 initial_steps = 480
 sim_steps = 14400
+
+dt = 1/240
+sample_dt = 1 # interval between each measurement in seconds
+sample_step = sample_dt/dt # in steps
+num_samples = int(sim_steps/sample_step-1)
 
 mode = p.POSITION_CONTROL
 maxforce = [5, 5, 5, 5]
@@ -197,8 +202,9 @@ for leg_angle in leg_angle_arr:
     p.resetSimulation()
     p.setGravity(0, 0, -10)
     planeId = p.loadURDF("plane.urdf")
-    p.changeDynamics(planeId, -1, contactStiffness=1500, contactDamping=30)
-    boxId = p.loadURDF(f"same_leg_urdf/backup/{leg_angle}_degree.urdf", cubeStartPos, cubeStartOrientation)
+    # p.changeDynamics(planeId, -1, contactStiffness=1500, contactDamping=30)
+    boxId = p.loadURDF(
+        f"same_leg_urdf/{leg_angle}_degree.urdf", cubeStartPos, cubeStartOrientation)
     # boxId = p.loadURDF("same_leg_urdf/24_degree.urdf", cubeStartPos, cubeStartOrientation)
     # p.changeDynamics(boxId, 1, contactStiffness = 1000, contactDamping = 1000)
 
@@ -218,9 +224,12 @@ for leg_angle in leg_angle_arr:
     #     WalkingTrot(parameter[0], p_stance_mid=back_stance_mid, contact_angle=back_contact_angle),  # back right
     #     WalkingTrot(parameter[0], p_stance_mid=front_stance_mid, contact_angle=front_contact_angle)]  # front right
     gaits = [  # rotate
-        WalkingTrot(0, p_stance_mid=front_stance_mid, contact_angle=front_contact_angle),  # front left
-        WalkingTrot(0, p_stance_mid=back_stance_mid, contact_angle=back_contact_angle),  # back left
-        WalkingTrot(parameter[0], p_stance_mid=back_stance_mid, contact_angle=back_contact_angle),  # back right
+        WalkingTrot(0, p_stance_mid=front_stance_mid,
+                    contact_angle=front_contact_angle),  # front left
+        WalkingTrot(0, p_stance_mid=back_stance_mid,
+                    contact_angle=back_contact_angle),  # back left
+        WalkingTrot(parameter[0], p_stance_mid=back_stance_mid,
+                    contact_angle=back_contact_angle),  # back right
         WalkingTrot(parameter[0], p_stance_mid=front_stance_mid, contact_angle=front_contact_angle)]  # front right
     # jointPosition = np.zeros(1440)
     # jointVelocity = np.zeros(1440)
@@ -230,12 +239,12 @@ for leg_angle in leg_angle_arr:
     # 2 right_front
     # 3 right_back
     # 4 left_back
-    T_pos = [0 for _ in range(4)]
-    T_test = [0 for _ in range(4)]
-    counter = [0 for _ in range(4)]
-    pos_tmp = [[0 for i in range(2)] for j in range(6)]
+    T_pos = np.zeros(4)#[0 for _ in range(4)]
+    T_test = np.zeros(4)#[0 for _ in range(4)]
+    counter = np.zeros(4)#[0 for _ in range(4)]
+    pos_tmp = np.zeros((2,6))#[[0 for i in range(2)] for j in range(6)]
     num = 0
-    velocity = [0 for _ in range(5)]
+    velocity = np.zeros(num_samples)#[0 for _ in range(num_samples)]
 
     T_test[0] = gaits[0].GetPos(0, w, parameter[1])
     T_test[1] = -gaits[3].GetPos(0, w, parameter[1])
@@ -255,42 +264,41 @@ for leg_angle in leg_angle_arr:
 
     for i in range(sim_steps):
         p.stepSimulation()
-
-        T_pos[0] = gaits[0].GetPos(i / 240, w, parameter[1])
-        T_pos[1] = gaits[3].GetPos(i / 240, w, parameter[1])
-        T_pos[2] = gaits[2].GetPos(i / 240, w, parameter[4])
-        T_pos[3] = gaits[1].GetPos(i / 240, w, parameter[4])
+        t = i*dt
+        T_pos[0] = gaits[0].GetPos(t, w, parameter[1])
+        T_pos[1] = gaits[3].GetPos(t, w, parameter[1])
+        T_pos[2] = gaits[2].GetPos(t, w, parameter[4])
+        T_pos[3] = gaits[1].GetPos(t, w, parameter[4])
         for a in range(4):
             if T_pos[a] < (T_test[a] - 3):
                 counter[a] += 1
             T_test[a] = T_pos[a]
 
-        T_pos[0] = gaits[0].GetPos(i / 240, w, parameter[1]) + TWO_PI * counter[0]
-        T_pos[1] = -gaits[3].GetPos(i / 240, w, parameter[1]) - TWO_PI * counter[1]
-        T_pos[2] = -gaits[2].GetPos(i / 240, w, parameter[4]) - TWO_PI * counter[2]
-        T_pos[3] = gaits[1].GetPos(i / 240, w, parameter[4]) + TWO_PI * counter[3]
+        T_pos[0] =  gaits[0].GetPos(t, w, parameter[1]) + TWO_PI * counter[0]
+        T_pos[1] = -gaits[3].GetPos(t, w,parameter[1]) - TWO_PI * counter[1]
+        T_pos[2] = -gaits[2].GetPos(t, w,parameter[4]) - TWO_PI * counter[2]
+        T_pos[3] =  gaits[1].GetPos(t, w, parameter[4]) + TWO_PI * counter[3]
 
         p.setJointMotorControlArray(boxId, [0, 1, 2, 3], controlMode=mode,
-                                    targetPositions=[T_pos[0], T_pos[1], T_pos[2], T_pos[3]],
+                                    targetPositions=[
+                                        T_pos[0], T_pos[1], T_pos[2], T_pos[3]],
                                     forces=maxforce)
-        if i == 2399: # Start calculate after 10 seconds
+        if i == sample_step-1:  # Start calculate after 10 seconds
             PosStart, OrnStart = p.getBasePositionAndOrientation(boxId)
-        if (((i + 1) % 2400 == 0) and i > 2400):
+        elif ((i + 1) % sample_step == 0):
             PosTest, OrnTest = p.getBasePositionAndOrientation(boxId)
-            velocity[num] = np.sqrt(pow(PosStart[0] - PosTest[0], 2) + pow(PosStart[1] - PosTest[1], 2)) / 10
-            # velocity[num] = (PosTest[0] - PosStart[0]) / 10
+            velocity[num] = np.sqrt(
+                pow(PosStart[0] - PosTest[0], 2) + pow(PosStart[1] - PosTest[1], 2)) / sample_dt
             PosStart = PosTest
             num += 1
         #time.sleep(1. / 240.)
     #PosEnd, OrnEnd = p.getBasePositionAndOrientation(boxId)
 
-    for i in range(5):
+    for i in range(num_samples):
         print(velocity[i])
     result.extend([(leg_angle, v) for v in velocity])
 result = np.asarray(result)
 # print(result)
 
 df = pd.DataFrame(result, columns=["leg_angle [deg]", "velocity [m/s]"])
-df.to_csv("vel_alternate_leg.csv", index=False)
-
-
+df.to_csv("vel_alternate.csv", index=False)
